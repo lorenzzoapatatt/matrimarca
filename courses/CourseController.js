1b29daca74a6d3774460db7da898ba75974bb018
lorenzzoapatatt <lorenzzoapatatt2209@gmail.com>
2026-04-03 19:36:41 -0300

    edição de tabelas terminada(falta apenas botar isso em todos os formularios)

--- courses/CourseController.js
+++ courses/CourseController.js
@@ -15,11 +15,59 @@ router.post("/courses/save", (req, res) => {
       nome: nome,
       cargaHoraria: cargaHoraria,
     }).then(() => {
-      res.redirect("/");
+      res.redirect("/admin/courses");
     });
   } else {
     res.redirect("/admin/courses/new");
   }
+});
+
+router.get("/admin/courses", (req, res) => {
+  Course.findAll().then((courses) => {
+    res.render("admin/courses/index", { courses: courses });
+  });
+});
+
+router.post("/courses/delete", (req, res) => {
+  var id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Course.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect("/admin/courses");
+      });
+    } else {
+      // nao for um numero
+      res.redirect("/admin/courses");
+    }
+  } else {
+    //NULL
+    res.redirect("/admin/courses");
+  }
+});
+
+router.get("/admin/courses/edit/:id", (req, res) => {
+  var id = req.params.id;
+
+  //ve se é um numero
+  if (isNaN(id)) {
+    res.redirect("/admin/courses");
+  }
+
+  Course.findByPk(id)
+    .then((courses) => {
+      if (courses != undefined) {
+        res.render("admin/courses/edit", { courses: courses });
+      } else {
+        res.redirect("/admin/courses");
+      }
+    })
+    .catch((error) => {
+      res.redirect("/admin/courses");
+    });
 });
 
 module.exports = router;
